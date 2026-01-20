def6a49d82d588e1cd3620aceef657391b4bb5c3
Jenyyk <jendotron2@gmail.com>
2026-01-20 11:25:28 +0100

    chore: format and lint

--- js/iframeScript.js
+++ js/iframeScript.js
@@ -1,44 +1,45 @@
-const frameParent = document.getElementById('frameParent');
-const timerElement = document.getElementById('frameTimer');
+const frameParent = document.getElementById("frameParent");
+const timerElement = document.getElementById("frameTimer");
 const sources = [
   "https://jenyyk.github.io",
-  "https://maturak26ab.cz"
-]
+  "https://maturak26ab.cz",
+];
 // starts at -1, because it is immediately incremented to 0
 let index = -1;
-let frames = [];
+const frames = [];
 let hovered = false;
-let animation;
 
 function createIframe(src) {
-    const iframe = document.createElement('iframe');
-    iframe.src = src;
-    return iframe;
+  const iframe = document.createElement("iframe");
+  iframe.src = src;
+  return iframe;
 }
 
 const circleIntervalTime = 8000; // ms
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   for (s of sources) {
-    let frame = createIframe(s);
+    const frame = createIframe(s);
     frames.push(frame);
     frameParent.insertBefore(frame, frameParent.firstChild);
   }
-  circleFrameForward()
-  animateTimer()
+  circleFrameForward();
+  animateTimer();
   setInterval(circleFrameForwardInterval, circleIntervalTime);
 });
 
 function circleFrameForwardInterval() {
-  animateTimer()
+  animateTimer();
   // we want to animate the timer to keep it synced, but not actually display the next iframe
-  if (hovered) { return; }
-  circleFrameForward()
+  if (hovered) return;
+  circleFrameForward();
 }
 
 function circleFrameForward() {
   index++;
-  frames[index % frames.length].style.display = 'block';
-  try { frames[(index - 1) % frames.length].style.display = 'none'; } catch { }
+  frames[index % frames.length].style.display = "block";
+  try {
+    frames[(index - 1) % frames.length].style.display = "none";
+  } catch { /* give up */ }
 }
 function animateTimer() {
   animation = timerElement.animate([
@@ -47,18 +48,18 @@ function animateTimer() {
     },
     {
       backgroundPosition: "0%",
-    }
+    },
   ], {
     duration: circleIntervalTime,
     iterations: 1,
-    easing: 'ease-in-out',
-  })
+    easing: "ease-in-out",
+  });
 }
 
-frameParent.addEventListener('mouseover', () => {
+frameParent.addEventListener("mouseover", () => {
   hovered = true;
 });
 
-frameParent.addEventListener('mouseout', () => {
+frameParent.addEventListener("mouseout", () => {
   hovered = false;
 });
